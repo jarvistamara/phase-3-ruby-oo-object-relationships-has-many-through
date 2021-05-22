61a0d77e146b3beda0a5b19d99e43ff4c626ae09
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-05-22 13:12:28 -0400

    something is broken, will revisit

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        self.meals.sort {|x, y| x.tip <=> y.tip }[self.meals.length-1].customer
+    end
+
+
+
 
 end
